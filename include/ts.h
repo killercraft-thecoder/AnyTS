7b39366eea0bef2442564f2f9de4bc71300e6c1b
killercraft-thecoder <Anonymous>
2025-09-05 15:13:22 -0700

    U

--- include/ts.h
+++ include/ts.h
@@ -10,39 +10,56 @@
 namespace TS
 {
 
+    /** 
+     * @struct
+     * @short Type Error
+    */
     struct TypeError
     {
-        size_t line;
-        std::string message;
+        size_t line; // Line Error Occured on
+        std::string message;  // Error Message
     };
 
+    /** 
+     * @struct
+     * @short Runtime Error
+    */
     struct RuntimeError {
-        size_t line;
-        std::string file;
-        std::string message;
+        size_t line; // Line Error Occured on
+        std::string file; // File Error Happened in.
+        std::string message; // Error Message
+    };
+
+    /** 
+     * @struct
+     * @short Error
+    */
+    struct Error {
+        size_t line; // Line Error Occured on
+        std::string message;  // Error Message
     };
 
     // --- Supported Value Types ---
     enum class ValueType
     {
-        Number,
-        String,
-        Boolean,
-        Null,
-        Undefined,
-        NaN,
+        Number, // Number (double)
+        String, // String (std::string)
+        Boolean, // boolean (bool)
+        Null, // NULL
+        Undefined, // Undefined
+        NaN, // NaN
     };
 
     // --- Value Representation ---
     struct Value
     {
-        ValueType type;
-        std::variant<double, std::string, bool> data;
-
+        ValueType type; // Current Type
+        std::variant<double, std::string, bool> data; // Current Value
+        
         Value(); // Null by default
-        Value(double num);
-        Value(const std::string &str);
-        Value(bool b);
+        Value(double num); // Create a TS::Value with a number.
+        Value(const std::string &str); // Create a TS::Value with a string.
+        Value(bool b); // Create a TS::Value with a boolean.
 
         std::string toString() const;
         double toNumber() const;
@@ -53,9 +70,26 @@ namespace TS
     using Environment = std::unordered_map<std::string, Value>;
 
     // --- Environment Helpers ---
-    bool setVar(Environment &env, const std::string &name, const Value &value);
+    /** 
+     * @fn
+     * @short Set Varible.
+    */
+    bool setVar(Environment &env, const std::string &name, const Value &value); 
+    /** 
+     * @fn
+     * @short Get Varible.
+    */
     std::optional<Value> getVar(const Environment &env, const std::string &name);
+    /** 
+     * @fn
+     * @short Var Exists?
+     * @returns wheter the var exists in the current enviroment.
+    */
     bool varExists(const Environment &env, const std::string &name);
-    std::vector<TypeError> checkTypesInSource(const std::string &source);
+    /** 
+     * @fn
+     * @short Check For type Errors.
+    */
+    std::vector<TypeError> checkTypesInSource(const std::string &source); 
 
 } // namespace TS
